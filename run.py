96974eb4851c4c246970f34f1ac7fe117b19707d
khanhdocnope <khanh4346k9@gmail.com>
2026-08-28 16:22:56 +0700

    fix: redirect stdout/stderr for windowed mode

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 import sys
 import os
 
+# Fix: PyInstaller windowed mode sets stdout/stderr to None
+if sys.stdout is None:
+    sys.stdout = open(os.devnull, 'w')
+if sys.stderr is None:
+    sys.stderr = open(os.devnull, 'w')
+
 # Add parent directory to path
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
